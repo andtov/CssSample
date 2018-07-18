f4aff45d90c2ad7b4bd1c7c07637b2815857aedf
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2018-07-17 20:26:25 -0500

    [Add] EjercicioIntervalo

--- Ejercicios/primer-js/app4.js
+++ Ejercicios/primer-js/app4.js
@@ -9,3 +9,12 @@ function elementIterator(item, index) {
     item.style.background = "#3f3f3f";
     item.style.color = 'white';
 }
+
+let intervalo = setInterval(function() {
+console.log('intervalo');
+}, 1000);
+
+setTimeout(() => {
+    console.log('Entro aquí');
+    clearInterval(intervalo);
+}, 5000);
